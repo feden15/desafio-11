8468a5c05499f4204eaa0cba116a9481b0f34cc0
Federico Nova <fedenova8@gmail.com>
2025-01-03 15:48:42 -0300

    Comenzamos a programar en main.js. Definimos las constantes pantalla y botones. Hicimos que, si se clickea en el input, el valor mostrado se reinicie a 0

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 import './style.css'
+
+const pantalla = document.querySelector('.pantalla');
+const botones = document.querySelectorAll('.boton');
+
+pantalla.addEventListener('click', () => {
+    operacion = 0;
+    pantalla.value = '0';
+});
