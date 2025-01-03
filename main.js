68e3bb875323c85e435225bdf976dfbc6c220c5f
Federico Nova <fedenova8@gmail.com>
2025-01-03 17:40:07 -0300

    Hicimos funcional la calculadora por completo, falta agregar los casos donde no se tendría que poder ejecutar la operación

--- main.js
+++ main.js
@@ -1,9 +1,32 @@
 import './style.css'
 
-const pantalla = document.querySelector('.pantalla');
-const botones = document.querySelectorAll('.boton');
+const pantalla = document.getElementById('pantalla');
+const botones = document.querySelectorAll('button');
+
+let operacion = 0;
 
 pantalla.addEventListener('click', () => {
     operacion = 0;
     pantalla.value = '0';
 });
+
+botones.forEach(boton => {
+    boton.addEventListener('click', () => {
+
+        const valor = boton.textContent;
+        
+        if (valor === '=') {
+            operacion = eval(operacion)
+            pantalla.value = operacion
+        } else if ((operacion === 0) && (valor === '0')) {
+            pantalla.value = '0'
+        } else if (operacion === 0) {
+            operacion = valor
+            pantalla.value = operacion
+        } else {
+            operacion = operacion + valor
+            pantalla.value = operacion
+        }
+
+    });
+})
